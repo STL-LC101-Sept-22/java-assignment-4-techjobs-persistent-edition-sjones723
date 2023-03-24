7239e324de0a1d7af1c4e0cc5d23d1a4fb943be0
Sydney Jones <sydneyjones87@gmail.com>
2023-03-24 18:23:41 -0500

    failing @Joined annotation check testing use

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,7 +1,10 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.JobData;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +20,9 @@ public class EmployerController {
 
     @Autowired
     private EmployerRepository employerRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
 
     /* Add an index method that responds at /employers with a list of all employers in the database. This method should use the template employers/index.
      */
@@ -46,12 +52,15 @@ public class EmployerController {
 
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
+        Iterable<Job> jobs;
 
 
         Optional<Employer> optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
+            jobs = JobData.findByValue(String.valueOf(employer), jobRepository.findAll());
+            model.addAttribute("jobs", jobs);
             return "employers/view";
         } else {
             return "redirect:../";
